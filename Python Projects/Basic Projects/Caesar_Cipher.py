0263c3ce8099e33665ab53e405eb04be61c99d7f
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-10 12:34:27 -0500

    Update Caesar_Cipher.py

--- Python Projects/Basic Projects/Caesar_Cipher.py
+++ Python Projects/Basic Projects/Caesar_Cipher.py
@@ -10,14 +10,36 @@ class CaesarCipher:
     """Initialize the phrase and the shift value to use"""
     self.phrase = phrase
     self.shift = shift
-    new_phrase = []
+    self.new_phrase = []
+    self.character_list = []
+    self.new_string = ''
+
+  def split_phrase(self):
+    for each_character in self.phrase:
+      self.character_list.append(each_character)
+    return self.character_list
+
 
   def cipher_message(self):
     """This converts a message into a ciphered code"""
-    list_from_string = self.phrase.split('')
-    for each_letter in self.phrase:
+    list_from_string = self.split_phrase()
+    for each_letter in list_from_string:
       if each_letter in lower_alphabet:
-        new_phrase.append(lower_alphabet[indexOf(each_letter) + self.shift])
+        self.new_phrase.append(lower_alphabet[lower_alphabet.index(each_letter) + self.shift])
+      elif each_letter in upper_alphabet:
+        self.new_phrase.append(upper_alphabet[upper_alphabet.index(each_letter) + self.shift])
+      else: 
+        self.new_phrase.append(each_letter)
+    
+    return self.new_phrase
 
-  
+  def join_phrase(self):
+    ciphered_list = self.cipher_message()
+
+    return self.new_string.join(ciphered_list)
+
+
+test = CaesarCipher('Hello World!', 3)
+
+print(test.join_phrase())
 
