9b2d045097b22cb9320c9b213a60368e8813429c
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-12 17:35:09 -0500

    Update Sub_Strings.py

--- Python Projects/Basic Projects/Sub_Strings.py
+++ Python Projects/Basic Projects/Sub_Strings.py
@@ -5,22 +5,19 @@ class SubString:
     """Initialize the phrase to apply the substrings method"""
     #self.phrase = phrase
     self.list_words = list_words # this one is a list 
-    self.dictionary_words = dict()
-    self.test_list = []
-    self.list_strings = []
+    self.dictionary_words = dict() # this is the dictionary with count for every word in the list_words
+    self.test_list = [] # this one stores the phrase truned into a list
+    self.sorted_dictionary = dict() # stores the dictionary sorted alphabetically
     
 
   def substrings(self):
     """Search for how many times a word appears in the string"""
     self.phrase = input("Enter you phrase:").lower()
+
+    # Creates a list of every word in the phrase
     self.test_list = self.phrase.split()
     print(self.test_list)
-    # Create a list from every match it founds in the phrase
-    """for each in self.list_words:
-      for each_word in self.test_list:
-        if each in each_word:
-          self.list_strings.append(each)"""
-    
+        
     # loop the found matches and create a dictionary counting them
     for item in self.test_list:
       for item2 in self.list_words:
@@ -29,12 +26,18 @@ class SubString:
             self.dictionary_words[item2] = self.dictionary_words[item2] + 1
         except KeyError:
           self.dictionary_words[item2] = 1
-           
 
-    return self.dictionary_words
+    # Sorted dictionary
+    
+    for key, values in sorted(self.dictionary_words.items()):
+      self.sorted_dictionary[key] = values
 
+    return self.sorted_dictionary
+
+# create a list of words to compare
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+# Instance of Substring
 test_substring = SubString(dictionary)
 
 print(test_substring.substrings())
