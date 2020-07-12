7a7bee7842cdd6a103ffc0a2ea30086aa98e685c
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-07-12 17:27:48 -0500

    Update Sub_Strings.py

--- Python Projects/Basic Projects/Sub_Strings.py
+++ Python Projects/Basic Projects/Sub_Strings.py
@@ -6,17 +6,29 @@ class SubString:
     #self.phrase = phrase
     self.list_words = list_words # this one is a list 
     self.dictionary_words = dict()
-    self.new_list = []
+    self.test_list = []
+    self.list_strings = []
+    
 
   def substrings(self):
     """Search for how many times a word appears in the string"""
-    self.phrase = input("Enter you phrase:")
-    for word in self.list_words:
-      try:
-        if word in self.phrase:
-          self.dictionary_words[word] = self.dictionary_words[word] + 1
-      except KeyError:
-        self.dictionary_words[word] = 1
+    self.phrase = input("Enter you phrase:").lower()
+    self.test_list = self.phrase.split()
+    print(self.test_list)
+    # Create a list from every match it founds in the phrase
+    """for each in self.list_words:
+      for each_word in self.test_list:
+        if each in each_word:
+          self.list_strings.append(each)"""
+    
+    # loop the found matches and create a dictionary counting them
+    for item in self.test_list:
+      for item2 in self.list_words:
+        try:
+          if item2 in item:
+            self.dictionary_words[item2] = self.dictionary_words[item2] + 1
+        except KeyError:
+          self.dictionary_words[item2] = 1
            
 
     return self.dictionary_words
