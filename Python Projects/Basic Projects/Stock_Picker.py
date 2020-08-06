c16f9b7ac7b7e37d7537470d4d61f70329d33fc4
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-08-06 15:58:53 -0500

    Update Stock_Picker.py

--- Python Projects/Basic Projects/Stock_Picker.py
+++ Python Projects/Basic Projects/Stock_Picker.py
@@ -1,13 +1,18 @@
 list_prices = [17,3,6,9,15,8,6,1,10]
-newlist = list()
-i = 0
+list_sorted = sorted(list_prices, reverse=True)
+best_days = list()
+profit = 0
 
-while i<len(list_prices):
-  if list_prices[i-1] < list_prices[i]:
-    newlist.append(list_prices[i])
+for sell_price in list_prices:
+  for buy_price in list_sorted:
+    if buy_price < sell_price and list_prices.index(buy_price) < list_prices.index(sell_price) and sell_price-buy_price>profit:
+      best_days.extend([list_prices.index(buy_price), list_prices.index(sell_price)])
+      profit = sell_price - buy_price
 
-  i += 1
 
-print(newlist)
+best_days.sort()
+print(best_days)
+
+print(f"The best day to buy is day {best_days[0]+1} and the best to sell is day {best_days[-1]+1}.\nProfit: {profit}")
 
 
