7e5e337830ee81b41892c9c6665299f6f5e0cb7b
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2020-12-14 20:37:43 -0500

    objects

---
+++ Udemy-Courses/Net Ninja Javascript/Objects/math_object.js
@@ -0,0 +1,3 @@
+console.log(Math);
+
+//That returns the function of Math with all the available functions in the form of an object Math{...}
--- Udemy-Courses/Net Ninja Javascript/Objects/objects.js
+++ Udemy-Courses/Net Ninja Javascript/Objects/objects.js
@@ -1,16 +1,28 @@
-//object literal
+//object literal. You can even add functions as values
 
 let user = {
   name: 'Crystal',
   age: 30,
   email: 'crystal3@netninja.com',
   location: 'Berlin',
-  blogs: ['history of america but not US', 'how to grow kids'],
+  /* blogs: ['history of america but not US', 'how to grow kids'], */
+  blogs: [
+    //An array of objects
+    {title: 'history of america but not US', likes: 30,},
+    {title: 'how to grow kids', likes: 50,},
+  ],
   login: function(){
     console.log('the user logged in');
   },
   logout: function(){
     console.log('the user logged out');
+  },
+  logBlogs: function(){
+    //this keyword is used to refer to this, actually this object. this refers to the object itself
+    console.log(`The user ${this.name} has the following blogs:`);
+    this.blogs.forEach(blog => {
+      console.log(blog.title, blog.likes);
+    });
   },
 };
 
